7330b54ac4164f913bd4931907681886225e36f2
Allegromare <allegromare@gmail.com>
2022-08-04 10:27:48 +0200

    Lecture 1 - exercise 1

--- cs50p/01 Conditionals/1_DeepThought.py
+++ cs50p/01 Conditionals/1_DeepThought.py
@@ -1,9 +1,13 @@
 """
-In deep.py, implement a program that prompts the user for the answer to the Great Question of Life, the Universe and Everything, outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two. Otherwise output No.
+In deep.py, implement a program that prompts the user for the 
+answer to the Great Question of Life, the Universe and Everything, 
+outputting Yes if the user inputs 42 or (case-insensitively) 
+forty-two or forty two. Otherwise output No.
 
 Hints
 -----
-No need to convert the user’s input to an int if you check for equality with "42", a str, rather than 42, an int!
+No need to convert the user’s input to an int if you check for 
+equality with "42", a str, rather than 42, an int!
 It’s okay if your output or the user’s wraps onto multiple lines.
 
 Example
@@ -22,3 +26,17 @@ What is the Answer to the Great Question of Life, the Universe, and Everything? 
 No                                                                                                 
 $       
 """
+
+response = input(
+    "What is the Answer to the Great Question of Life, the Universe, and Everything? "
+)
+
+if (
+    response == "42"
+    or response.casefold() == "forty-two"
+    or response.casefold() == "forty two"
+    or response.casefold() == "fortytwo"
+):
+    print("Yes")
+else:
+    print("No")
